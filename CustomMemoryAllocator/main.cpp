a2ea41f77153e8881aef07b4f88bda8e289cf44a
Efe Sinop <efeanp@gmail.com>
2026-03-18 15:16:05 +0300

    Update main.cpp

--- CustomMemoryAllocator/main.cpp
+++ CustomMemoryAllocator/main.cpp
@@ -2,7 +2,7 @@
 #include "MemoryAllocator.hpp"
 
 int main() {
-    // 64 byte'lik 10 yer ayiriyoruz. 9800X3D icin cocuk oyuncagi!
+    
     MemoryPool pool(64, 10);
 
     void* adres = pool.allocate();
